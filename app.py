5194f7d7aeb264022434bea624af8add5bf112e1
Ukayria <chinedujoseph9927@gmail.com>
2026-03-20 06:56:46 +0100

    fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 from flask import Flask
 
 template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__, template_folder=template_dir, static_folder='Static', static_url_path='/static')
 
 BASE_DIR = os.path.dirname(__file__)
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
